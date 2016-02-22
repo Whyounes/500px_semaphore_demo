a68818e7f21982c9d37b0e232054d34f7d2fef28
RAFIE Younes <younes.rafie@gmail.com>
2016-02-22 01:00:17 +0000

    Added nav brand

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -24,6 +24,7 @@
         <div class="container">
 
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <a class="navbar-brand navbar-right" href="#">Semaphore Demo</a>
                 <ul class="nav navbar-nav">
                     <li>
                         <a href="/">Home</a>
